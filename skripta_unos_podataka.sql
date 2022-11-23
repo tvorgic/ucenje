9a532fdc8d516c0e89ecc193a049b29dd1381a07
Tonko Vorgić <117771377+tvorgic@users.noreply.github.com>
2022-11-23 15:47:02 +0100

    path

--- skripta_unos_podataka.sql
+++ skripta_unos_podataka.sql
@@ -1,6 +1,6 @@
 # LJESTVE (HASH) JE KOMENTAR I ON SE NE IZVODI
 # KOMANDA KOJA SE LIJEPI U SHELL (MACOS) JE U LINIJI ISPOD
-# /Applications/XAMPP/xamppfiles/bin/./mysql -uroot < /Users/marko/Documents/EdunovaPP26/Ucenje-EdunovaPP26/skripta.sql
+# /Applications/XAMPP/xamppfiles/bin/./mysql -uroot < C:\Users\Tonko\Documents\edunovapp26\SQL\ucenje\skripta_unos_podataka.sql
 ​
 drop database if exists edunovapp26;
 # BRISE STARU BAZU PODATAKA AKO VEC POSTOJI DA BI MOGLI NAPRAVITI NOVU VERZIJU
